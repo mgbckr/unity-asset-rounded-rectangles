7009daadd3a001c8ed13c5045d842b6f5f177dd5
Martin Becker <1241516+mgbckr@users.noreply.github.com>
2025-02-16 14:08:36 +0100

    Add namespace

--- Scripts/Set Content.cs
+++ Scripts/Set Content.cs
@@ -1,49 +1,52 @@
 using UnityEngine;
 
-public class SetContent : MonoBehaviour
-{
-    private GameObject plane;
 
-    [SerializeField]
-    private Texture2D texture;
-
-    [SerializeField]
-    private Color color;
-
-    void Start()
+namespace RoundedRectangles {
+    public class SetContent : MonoBehaviour
     {
-        if (texture != null)
+        private GameObject plane;
+
+        [SerializeField]
+        private Texture2D texture;
+
+        [SerializeField]
+        private Color color;
+
+        void Start()
         {
-            Debug.Log("Setting texture"); 
-            GetComponent<Renderer>().material.SetTexture("_MainTex", texture);
-        }
-
-        if (color != null)
-        {
-             // Get the Mesh from the MeshFilter component
-            MeshFilter meshFilter = GetComponent<MeshFilter>();
-            Mesh mesh = meshFilter.mesh;
-
-            // Retrieve the vertices of the mesh
-            Vector3[] vertices = mesh.vertices;
-            Color[] colors = new Color[vertices.Length];
-
-            // Set a color for each vertex. For example, using the vertex's position to create a color.
-            for (int i = 0; i < vertices.Length; i++)
+            if (texture != null)
             {
-                // This example creates a gradient color based on the x, y, and z coordinates
-                colors[i] = color;
+                Debug.Log("Setting texture"); 
+                GetComponent<Renderer>().material.SetTexture("_MainTex", texture);
             }
 
-            // Apply the color array to the mesh
-            mesh.colors = colors;
+            if (color != null)
+            {
+                // Get the Mesh from the MeshFilter component
+                MeshFilter meshFilter = GetComponent<MeshFilter>();
+                Mesh mesh = meshFilter.mesh;
 
+                // Retrieve the vertices of the mesh
+                Vector3[] vertices = mesh.vertices;
+                Color[] colors = new Color[vertices.Length];
+
+                // Set a color for each vertex. For example, using the vertex's position to create a color.
+                for (int i = 0; i < vertices.Length; i++)
+                {
+                    // This example creates a gradient color based on the x, y, and z coordinates
+                    colors[i] = color;
+                }
+
+                // Apply the color array to the mesh
+                mesh.colors = colors;
+
+            }
         }
-    }
 
-    // Update is called once per frame 
-    void Update()
-    {
-        
+        // Update is called once per frame 
+        void Update()
+        {
+            
+        }
     }
 }
