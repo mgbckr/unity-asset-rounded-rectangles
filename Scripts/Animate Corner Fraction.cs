7d79815ca46492a56580dd9fb406ece4e6af51cc
Martin Becker <1241516+mgbckr@users.noreply.github.com>
2025-04-05 15:32:51 +0200

    Animate corner fraction

--- Scripts/Animate Corner Fraction.cs
+++ Scripts/Animate Corner Fraction.cs
@@ -7,4 +7,21 @@ public class AnimateCornerFraction : MonoBehaviour
 
 
     }
+
+    void Update()
+    {
+
+
+
+        float pulse = Mathf.PingPong(Time.time * 3, 1f);
+        float scale = Mathf.Lerp(0f, 1f, pulse);
+
+        // animate the "End" property of the underlying shader
+        Material material = GetComponent<Renderer>().material;
+        material.SetFloat("_End", scale);
+
+
+
+
+    }
 }
