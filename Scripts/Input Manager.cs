a9a7d31c11f972dd444e7e4675c3e7dbb77dd033
Martin Becker <1241516+mgbckr@users.noreply.github.com>
2025-06-23 21:31:57 +0200

    Initial resizing functionality

--- Scripts/Input Manager.cs
+++ Scripts/Input Manager.cs
@@ -5,7 +5,7 @@ using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 using UnityEngine.InputSystem.LowLevel;
 
-public class MoveObjectOnInput : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
 
     private GameObject selectedObject;
@@ -31,6 +31,8 @@ public class MoveObjectOnInput : MonoBehaviour
                 if (touchData.targetObject != null && touchData.Kind != SpatialPointerKind.Touch)
                 {
                     Debug.Log("Touch interaction with: " + touchData.targetObject.name);
+
+                    // MOVE
                     if (touchData.targetObject.name == "Move")
                     {
 
@@ -46,6 +48,40 @@ public class MoveObjectOnInput : MonoBehaviour
                             lastPosition = touchData.interactionPosition;
                         }
                     }
+
+                    // RESIZE
+                    else if (touchData.targetObject.name == "Resize Right" || touchData.targetObject.name == "Resize Left")
+                    {
+                        if (touch.phase == TouchPhase.Began)
+                        {
+                            selectedObject = touchData.targetObject.transform.parent.parent.gameObject;
+                            lastPosition = touchData.interactionPosition;
+                        }
+                        else if (touch.phase == TouchPhase.Moved && selectedObject != null)
+                        {
+                            Vector3 deltaPosition = touchData.interactionPosition - lastPosition;
+                            selectedObject.transform.localScale += new Vector3(deltaPosition.x, deltaPosition.y, 0); // Resize only in the X and Y direction
+
+                            // // We need tp update the position as well
+                            // if (touchData.targetObject.name == "Resize Left")
+                            // {
+                            //     selectedObject.transform.position += new Vector3(
+                            //         - deltaPosition.x / 2,
+                            //         - deltaPosition.y / 2,
+                            //         0);
+                            // }
+                            // else if (touchData.targetObject.name == "Resize Right")
+                            // {
+                            //     selectedObject.transform.position += new Vector3(
+                            //         + deltaPosition.x / 2,
+                            //         - deltaPosition.y / 2,
+                            //         0);
+                            // }
+
+                            lastPosition = touchData.interactionPosition;
+                        }
+                    }
+
                 }
             }
         }
--- Scripts/Window.cs
+++ Scripts/Window.cs
@@ -3,50 +3,31 @@ using UnityEngine;
 public class Window : MonoBehaviour
 {
 
-    private GameObject controls;
     private GameObject frame;
-
-    // controls modes (enum): inactive, move, left, right, (close)
-    // inactive: no controls are active
-    // move: the window can be moved
-    // left: the window can be resized from the left
-    // right: the window can be resized from the right
-    // close: the window can be closed
-    public enum ControlsMode
-    {
-        Inactive,
-        Move,
-        Left,
-        Right,
-        Close
-    }
-    private ControlsMode controlsMode = ControlsMode.Inactive;
+    private GameObject controls;
+    private GameObject controls_center;
+    private GameObject controls_center_move;
+    private GameObject controls_center_close;
+    private GameObject controls_left;
+    private GameObject controls_right;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Find the controls GameObject iin the children of this GameObject
-        controls = transform.Find("Controls").gameObject;
-        Debug.Log("Controls: " + controls);
-
         // Find the frame GameObject in the children of this GameObject
         frame = transform.Find("Frame").gameObject;
+
+        // Find the controls GameObject iin the children of this GameObject
+        controls = transform.Find("Controls").gameObject;
+        controls_center = controls.transform.Find("Center").gameObject;
+        controls_center_move = controls_center.transform.Find("Move").gameObject;
+        controls_center_close = controls_center.transform.Find("Close").gameObject;
+        controls_left = controls.transform.Find("Left").gameObject;
+        controls_right = controls.transform.Find("Right").gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Material material = controls.GetComponent<Renderer>().material;
-        Color color = material.GetColor("_Color");
-
-        float speed = 1f;
-        float pulse = Mathf.PingPong(Time.time * speed, 1f);
-        float sawtooth = (Time.time % (1 / speed)) / (1 / speed);
-
-        // set segment position in material
-        float segmentPosition = sawtooth;
-        material.SetFloat("_Segment_Position", segmentPosition);
-        material.SetColor("_Color", new Color(color.r, color.g, color.b, pulse));
-        Debug.Log("Segment Position: " + segmentPosition);
     }
 }
